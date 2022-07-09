f254dc52dbe71c534e83825182bd5708ed73da44
rnycz <radeknycz98@gmail.com>
2022-07-09 14:02:49 +0200

    netlify server

---
+++ src/api.js
@@ -0,0 +1,23 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+const serverless = require('serverless-http')
+const router = express.Router()
+const app = express()
+
+router.get("/", (req, res) => {
+    res.set('Content-Type', 'text/html')
+    res.status(200)
+    res.send("<h1>Welcome on Server</h1>")
+  });
+  
+  
+  router.post('/update',(req,res) => {
+      res.json({
+          hello: "hit the POST!"
+        });
+  })
+  
+  app.use(`/.netlify/functions/api`, router);
+  
+  module.exports = app;
+  module.exports.handler = serverless(app);

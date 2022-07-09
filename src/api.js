699e15803ee725f0183ccd90c9021dca3e8db55f
rnycz <radeknycz98@gmail.com>
2022-07-09 14:37:22 +0200

    fixes

--- src/api.js
+++ src/api.js
@@ -4,22 +4,22 @@ const serverless = require('serverless-http')
 const router = express.Router()
 const app = express()
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.urlencoded())
 
-router.get("/", (req, res) => {
+/*router.get("/", (req, res) => {
     res.set('Content-Type', 'text/html')
     res.status(200)
     res.send("<h1>Welcome on Server</h1>")
     const rowNumber = req.body
     console.log("Rows: ", rowNumber)
-  });
+  });*/
   
   
-  router.post('/update',(req,res) => {
+  router.post('/',(req,res) => {
     const rowNumber = req.body
     console.log("Rows: ", rowNumber)
-    res.sendStatus(200);
+    res.end()
   })
   
   app.use(`/.netlify/functions/api`, router);
